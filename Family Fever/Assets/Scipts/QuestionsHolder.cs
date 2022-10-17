782a4f5a522a520b208108bdc7ac4f9094ca598a
1Naprestegard <naprestegard@gmail.com>
2022-10-17 09:00:05 -0700

    itsALIVE

--- Family Fever/Assets/Scipts/QuestionsHolder.cs
+++ Family Fever/Assets/Scipts/QuestionsHolder.cs
@@ -9,11 +9,11 @@ public class QuestionsHolder : MonoBehaviour
 {
     
     public string Question1 = "Name something in a person's closet that only comes out on special occasions?";
-    public string Question2 = "In horror movies, name a place teenagers always go when there is a killer on the loose?";
-    public string Question3 = "Name the most useful body part that begins with the letter \"L\" ";
-    public string Question4 = "Name something you have more of than Steve Harvey?";
-    public string Question5 = "Name something people win on game shows?";
-    public string Question6 = "Name something a burglar wouldn't want to see when they break into a house";
+    public string Question6 = "In horror movies, name a place teenagers always go when there is a killer on the loose?";
+    public string Question2 = "Name the most useful body part that begins with the letter \"L\" ";
+    public string Question3 = "Name something you have more of than Steve Harvey?";
+    public string Question4 = "Name something people win on game shows?";
+    public string Question5 = "Name something a burglar wouldn't want to see when they break into a house";
 
     private string Answer;
     
@@ -50,27 +50,27 @@ public class QuestionsHolder : MonoBehaviour
             Text DisplayText = GameObject.Find("Canvas/Text").GetComponent<Text>();
             DisplayText.text = Question1;
         }
-        else if (ValueStorage.QuestionNum == 2)
+        else if (ValueStorage.QuestionNum == 6)
         {
             Text DisplayText = GameObject.Find("Canvas/Text").GetComponent<Text>();
             DisplayText.text = Question2;
+        }
+        else if (ValueStorage.QuestionNum == 2)
+        {
+            Text DisplayText = GameObject.Find("Canvas/Text").GetComponent<Text>();
+            DisplayText.text = Question3;
         }
         else if (ValueStorage.QuestionNum == 3)
         {
             Text DisplayText = GameObject.Find("Canvas/Text").GetComponent<Text>();
-            DisplayText.text = Question2;
+            DisplayText.text = Question4;
         }
         else if (ValueStorage.QuestionNum == 4)
         {
             Text DisplayText = GameObject.Find("Canvas/Text").GetComponent<Text>();
-            DisplayText.text = Question4;
-        }
-        else if (ValueStorage.QuestionNum == 5)
-        {
-            Text DisplayText = GameObject.Find("Canvas/Text").GetComponent<Text>();
             DisplayText.text = Question5;
         }
-        else if (ValueStorage.QuestionNum == 6)
+        else if (ValueStorage.QuestionNum == 5)
         {
             Text DisplayText = GameObject.Find("Canvas/Text").GetComponent<Text>();
             DisplayText.text = Question6;
