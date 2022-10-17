f452cb114552c9839d3dbfdb2c04c6ed9fb082dc
1Naprestegard <naprestegard@gmail.com>
2022-10-17 09:07:35 -0700

    screaming

--- Family Fever/Assets/Scipts/QuestionsHolder.cs
+++ Family Fever/Assets/Scipts/QuestionsHolder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEditorInternal;
 
 public class QuestionsHolder : MonoBehaviour
 {
